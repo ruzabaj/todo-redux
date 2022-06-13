81cc0d8afedf7e4151c22692e0fd90ece5dabc6b
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-14 00:34:58 +0545

    add dispatch of react-redux

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { useState } from "react";
 import "../sass/todo.scss";
+import { useDispatch } from "react-redux";
 import { addTodo, deleteTodo , removeTodo } from "../action";
 const Todo = () => {
     const [inputValue , setInputvalue] = useState('');
-    
+    const dispatch=useDispatch;
+
   return (
     <div className="todo-outer">
     <div className="todo">
       <div className="heading">My Todo-s</div>
       <div className="input-todo">
         <input type="text" onChange={e=>setInputvalue(e.target.value)} value={inputValue}/>
-        <button type="submit" className="btn-add" onClick={}>Add</button>
+        <button type="submit" className="btn-add" onClick={()=>dispatch()}>Add</button>
       </div>
     </div>
     </div>
