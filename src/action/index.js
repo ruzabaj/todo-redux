08f4b4c568ef8357a48d80956cbb94ab4f378d67
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-13 23:48:18 +0545

    todo input and button

---
+++ src/action/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const addTodo = () => {
+  return (
+    type: 'ADD_TODO'
+  )
+}
+export const deleteTodo = () => {
+    return (
+      type: 'DELETE_TODO'
+    )
+  }
+  
+  export const removeTodo = () => {
+    return (
+      type: 'REMOVE_TODO'
+    )
+  }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import "../sass/todo.scss";
 
 const Todo = () => {
+    const [inputValue , setInputvalue] = useState('');
+    const inputText =()=>{
+        
+    }
   return (
-    <div> 
-        <div className="heading">My Todo-s</div>
-        <input type="text"/>
+    <div className="todo-outer">
+    <div className="todo">
+      <div className="heading">My Todo-s</div>
+      <div className="input-todo">
+        <input type="text" onChange="inputText"  value={inputValue}/>
+        <button type="submit" className="btn-add" >Add</button>
+      </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
