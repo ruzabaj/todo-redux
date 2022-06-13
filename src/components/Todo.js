e6ac24ac42803959d4c3c30beeeaaba320f35a08
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-13 21:54:35 +0545

    setup

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div> 
+        <div className="heading">My Todo-s</div>
+        <input type="text"/>
+    </div>
+  )
+}
+
+export default Todo
