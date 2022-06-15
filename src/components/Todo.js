7ec184a5d094ea244762501b5881bb6db6c49e87
userbaj <64317950+userbaj@users.noreply.github.com>
2022-06-15 22:33:25 +0545

    delete task updated

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,28 +1,49 @@
 import React from "react";
 import { useState } from "react";
 import "../sass/todo.scss";
-import { addTodo, deleteTodo , removeTodo } from "../action";
+import { addTodo, deleteTodo, removeTodo } from "../action";
 import { useSelector, useDispatch } from "react-redux";
 const Todo = () => {
-    const [inputValue , setInputvalue] = useState('');
-    const dispatch=useDispatch;
+  const [inputValue, setInputvalue] = useState("");
+  const List = useSelector((state) => state.todoReducer.dataList);
+  const dispatch = useDispatch;
 
   return (
     <div className="todo-outer">
-    <div className="todo">
-      <div className="heading">My Todo-s</div>
+      <div className="todo">
+        <div className="heading">My Todo-s</div>
 
-      <div className="input-todo">
-        <input type="text" onChange={e=>setInputvalue(e.target.value)} value={inputValue}/>
-        <button type="submit" className="btn-add" onClick={()=>dispatch(addTodo(inputValue), setInputvalue(''))}>Delete</button>
-      </div>
-      <div className="show-todo">
-        <div className="items">
-        <input type="text" onChange={e=>setInputvalue(e.target.value)} value={inputValue}/>
-        <button type="submit" className="btn-add" onClick={()=>dispatch(deleteTodo(inputValue), setInputvalue(''))}>Add</button> 
+        <div className="input-todo">
+          <input
+            type="text"
+            onChange={(e) => setInputvalue(e.target.value)}
+            value={inputValue}
+          />
+          <button
+            type="submit"
+            className="btn-add"
+            onClick={() => dispatch(addTodo(inputValue), setInputvalue(""))}
+          >
+            Delete
+          </button>
+        </div>
+        <div className="show-todo">
+          {List.map((i) => {
+            <div className="items" key={i.id}>
+              <h3>{i.data}</h3>
+              <button
+                type="submit"
+                className="btn-add"
+                onClick={() =>
+                  dispatch(deleteTodo(i.id))
+                }
+              >
+                Add
+              </button>
+            </div>;
+          })}
         </div>
       </div>
-    </div>
     </div>
   );
 };
